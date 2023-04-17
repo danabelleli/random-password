5c391be8f3d823ec0a14669772f1b9ee22c42a14
dannabelleli <belleli.danna@gmail.com>
2023-04-17 11:01:16 -0700

    changed most of the JS code, now the app is complete and working

--- script.js
+++ script.js
@@ -1,51 +1,49 @@
 // Assignment code here
-// arrays of the criteria
-var upperCaseChars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var lowerCaseChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var numericChars = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-var specialChars = ['!', '@', '#', '$', '%', '^', '&', '*', '-'];
+// stirngs of the criteria
+var upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var lowerCaseChars = 'abcdefghijklmnopqrstuvwxyz';
+var numericChars = '0123456789';
+var specialChars = '!@#$%^&*-';
 
 var generatePassword = function () {
   // Generating password length
   var lengthInput = Number(prompt('choose between 8-128 characters'));
-  for (var i = 0; i <= 100; i++) {
-    // making sure it's not below 8
-    if (lengthInput < 8) {
-      alert('must be at least 8 characters, try again');
-      lengthInput = Number(prompt('choose between 8-128 characters'));
-      // making sure it's not above 128
-    } else if (lengthInput > 128) {
-      alert('must be below 128 characters, try again');
-      lengthInput = Number(prompt('choose between 8-128 characters'));
-    }
+  while (lengthInput < 8 || lengthInput > 128) {
+    alert('must be between 8-128 characters, try again');
+    lengthInput = Number(prompt('choose between 8-128 characters'));
   }
-  console.log(lengthInput);
+
+  var selectedChars = '';
   // asking for character types
   var lowerCaseInput = prompt('do you want to include lowercase?').toLowerCase();
+  if (lowerCaseInput === 'yes') {
+    selectedChars += lowerCaseChars;
+    console.log(selectedChars);
+  }
   var upperCaseInput = prompt('do you want to include uppercase?').toLowerCase();
+  if (upperCaseInput === 'yes') {
+    selectedChars += upperCaseChars;
+    console.log(selectedChars);
+  }
   var numericInput = prompt('do you want to include numeric?').toLowerCase();
+  if (numericInput === 'yes') {
+    selectedChars += numericChars;
+    console.log(selectedChars);
+  }
   var specialInput = prompt('do you want to include special characters?').toLowerCase();
-
-  console.log(lowerCaseInput, upperCaseInput, numericInput, specialInput);
-
-  // option 1: all criteria wanted
-  if (lowerCaseInput === 'yes' && upperCaseInput === 'yes' && numericInput === 'yes' && specialInput === 'yes') {
-    // combining all arrays into 1 array
-    var allChars = upperCaseChars.concat(lowerCaseChars).concat(numericChars).concat(specialChars);
-    var password = '';
-    // generating random index from the array
-    for (var i = 0; i < lengthInput; i++) {
-      var randomNumber = allChars[Math.floor(Math.random() * allChars.length)];
-      console.log(randomNumber);
-    }
-
+  if (specialInput === 'yes') {
+    selectedChars += specialChars;
+    console.log(selectedChars);
   }
 
-
+  var password = '';
+  // generating random character
+  for (var i = 0; i < lengthInput; i++) {
+    var randomNumber = selectedChars[Math.floor(Math.random() * selectedChars.length)];
+    password += randomNumber;
+  }
+  return password;
 }
-
-
-
 
 
 // Get references to the #generate element
